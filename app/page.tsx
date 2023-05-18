5392303c431767ee39045ea3e026e244a300e0e5
Karl Cereno <kagc.dev@gmail.com>
2023-05-18 11:15:04 -0400

    Finish Home draft

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover and Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, ea.
+        Inventore, fuga nisi! Velit explicabo excepturi eos cum laborum dolor
+        soluta dolorem optio dolore ex?
+      </p>
+
+      {/* Feed */}
+    </section>
+  );
 };
 
 export default Home;
